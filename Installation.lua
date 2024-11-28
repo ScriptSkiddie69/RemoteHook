e66e17a97f8ecfb69933ee3e01036bb3a47dc403
ScriptSkiddie69 <110344732+ScriptSkiddie69@users.noreply.github.com>
2024-11-28 20:58:07 +0800

    Update Installation.lua

--- Installation.lua
+++ Installation.lua
@@ -1,7 +1,7 @@
 local makefolder = makefolder or warn('YOUR EXECUTOR DOES NOT SUPPORT MAKEFOLDER! ')
 local readfile = readfile or warn('YOUR EXECUTOR DOES NOT SUPPORT READFILE! ')
 local writefile = writefile or warn('YOUR EXECUTOR DOES NOT SUPPORT WRITEFILE! ')
-local install
+local install = "https://pastebin.com/raw/DKX7Aa9n"
 if isfolder('RemoteHook') then
     warn('3/3 - RemoteHook installed')
     else
@@ -10,12 +10,12 @@ if isfolder('RemoteHook') then
     warn('1/3 - Writing files into /RemoteHook')
     wait(1)
     writefile('RemoteHook//main.lua', '')
-    warn('2/3 - Setting up install link..')
+    warn('2/3 - Setting up dependencies..')
     install = "https://pastebin.com/raw/DKX7Aa9n"
     wait(1)
     warn('3/3 - Installing utilities into /RemoteHook')
     wait(1)
     appendfile('RemoteHook//main.lua', loadstring(game:HttpGet(install))())
     warn('RemoteHook intiated to use it go to documentation at: https://github.com/ScriptSkiddie69/RemoteHook')
-    warn('Or simply by defining local hook = loadstring(readfile('RemoteHook//main.lua'))() and you learn the rest')
+    warn("Or simply by defining local hook = loadstring(readfile('RemoteHook//main.lua'))() and you learn the rest')")
 end
